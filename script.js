e56d7ef7008189ab131848f775c3601478142cd3
petruzzelli-luca <petruzzelli.luca@iisgalvanimi.edu.it>
2025-03-26 10:45:35 +0000

    terzo commit

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 function startGame() {
-    myGamePiece.loadImages(runningImages);
+    myGamePiece.loadImages(runningImages, idleImage);
     myGameArea.start();
 }
+
+
 
 var myGamePiece = {
     speedX: 0,
@@ -11,6 +13,8 @@ var myGamePiece = {
     x: 10,
     y: 120,
     imageList: [],
+    imageListRunning:[],
+    imageListIdle:[],
     contaFrame: 0,
     actualFrame: 0,
     image: null,
@@ -27,18 +31,27 @@ var myGamePiece = {
         this.contaFrame++;
         if (this.contaFrame == 5) {
             this.contaFrame = 0;
-            this.actualFrame = (this.actualFrame + 1) % this.imageList.length;
+            this.actualFrame = (this.actualFrame + 100) % this.imageList.length;
             this.image = this.imageList[this.actualFrame];
         }
     },
 
-    loadImages: function(running) {
+    loadImages: function(running,idle) {
         for (let imgPath of running) {
             var img = new Image();
             img.src = imgPath;
-            this.imageList.push(img);
+            this.imageListRunning.push(img);
         }
-        this.image = this.imageList[this.actualFrame];
+        i =0;
+        for (let imgPath of idle) {
+            console.log(i++);
+            var img = new Image();
+            img.src = imgPath;
+            this.imageListIdle.push(img);
+        }
+        this.image = this.imageListRunning[this.actualFrame];
+        console.log (this.imageListRunning);
+        this.imageList  = this.imageListIdle;
     }
 };
 
@@ -55,10 +68,10 @@ var myGameArea = {
         document.body.insertBefore(this.canvas, document.body.childNodes[0]);
         this.interval = setInterval(updateGameArea, 1);
         window.addEventListener('keydown', function(e) {
-            myGameArea.keys[e.keyCode] = true;
+            myGameArea.keys[e.key] = true;
         });
         window.addEventListener('keyup', function(e) {
-            myGameArea.keys[e.keyCode] = false;
+            myGameArea.keys[e.key] = false;
         });
     },
 
@@ -82,11 +95,43 @@ function updateGameArea() {
     myGamePiece.speedX = 0;
     myGamePiece.speedY = 0;
 
-    if (myGameArea.keys[37]) { // left
+    myGamePiece.imageList = myGamePiece.imageListRunning;
+
+    /*switch (event.key) {
+        case "ArrowDown":
+          // Do something for "down arrow" key press.
+          break;
+        case "ArrowUp":
+          // Do something for "up arrow" key press.
+          break;
+        case "ArrowLeft":
+          // Do something for "left arrow" key press.
+          break;
+        case "ArrowRight":
+          // Do something for "right arrow" key press.
+          break;
+        case "Enter":
+          // Do something for "enter" or "return" key press.
+          break;
+        case " ":
+          // Do something for "space" key press.
+          break;
+        case "Escape":
+          // Do something for "esc" key press.
+          break;
+        default:
+          return; // Quit when this doesn't handle the key event.
+      }*/
+
+    if (myGameArea.keys["ArrowLeft"]) { // left
         myGamePiece.speedX = -1;
     }
-    if (myGameArea.keys[39]) { // right
+    else if (myGameArea.keys["ArrowRight"]) { // right
         myGamePiece.speedX = 1;
+    }
+    else
+    {
+        myGamePiece.imageList = myGamePiece.imageListIdle;
     }
     
 
@@ -95,4 +140,3 @@ function updateGameArea() {
 }
 
 
-startGame();
--- sprite.js
+++ sprite.js
@@ -1,16 +1,35 @@
 var runningImages = [
-    'santasprites/png/Walk (1).png', 
-    'santasprites/png/Walk (2).png',
-    'santasprites/png/Walk (3).png',
-    'santasprites/png/Walk (4).png',
-    'santasprites/png/Walk (5).png',
-    'santasprites/png/Walk (6).png',
-    'santasprites/png/Walk (7).png',
-    'santasprites/png/Walk (8).png',
-    'santasprites/png/Walk (9).png',
-    'santasprites/png/Walk (10).png',
-    'santasprites/png/Walk (11).png',
-    'santasprites/png/Walk (12).png',
-    'santasprites/png/Walk (13).png'
+    'santasprites/png/Run (1).png', 
+    'santasprites/png/Run (2).png',
+    'santasprites/png/Run (3).png',
+    'santasprites/png/Run (4).png',
+    'santasprites/png/Run (5).png',
+    'santasprites/png/Run (6).png',
+    'santasprites/png/Run (7).png',
+    'santasprites/png/Run (8).png',
+    'santasprites/png/Run (9).png',
+    'santasprites/png/Run (10).png',
+    'santasprites/png/Run (11).png'
   
   ];
+
+
+  var idleImage =[
+    'santasprites/png/Idle (1).png',
+    'santasprites/png/Idle (2).png',
+    'santasprites/png/Idle (3).png',
+    'santasprites/png/Idle (3).png',
+    'santasprites/png/Idle (4).png',
+    'santasprites/png/Idle (5).png',
+    'santasprites/png/Idle (6).png',
+    'santasprites/png/Idle (7).png',
+    'santasprites/png/Idle (8).png',
+    'santasprites/png/Idle (9).png',
+    'santasprites/png/Idle (10).png',
+    'santasprites/png/Idle (11).png',
+    'santasprites/png/Idle (12).png',
+    'santasprites/png/Idle (13).png',
+    'santasprites/png/Idle (14).png',
+    'santasprites/png/Idle (15).png',
+    'santasprites/png/Idle (16).png'
+  ]
